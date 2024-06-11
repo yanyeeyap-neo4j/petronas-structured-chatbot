7be217f7b3edcd9b28538504e44ad87c44684c72
yanyeeyap <yanyee.yap@neo4j.com>
2024-06-11 11:41:17 +0000

    Change PETRONAS logo pixel 10

--- ui/streamlit/pages/3_Chat.py
+++ ui/streamlit/pages/3_Chat.py
@@ -19,7 +19,7 @@ st.set_page_config(page_icon="🧠", layout="wide")
 # Display the uploaded image at the top and center
 st.markdown("""
     <div style='text-align: center;'>
-        <img src='data:image/png;base64,{}' width="300">
+        <img src='data:image/png;base64,{}' width="50">
     </div>
 """.format(st.image(petronas, use_column_width=True)), unsafe_allow_html=True)
 
