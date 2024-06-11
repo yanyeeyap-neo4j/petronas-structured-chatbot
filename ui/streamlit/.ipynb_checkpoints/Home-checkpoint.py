b114bf6b85d5b886a94c313f07ec5745a81b2c70
yanyeeyap <yanyee.yap@neo4j.com>
2024-06-11 11:27:23 +0000

    Change PETRONAS logo pixel 3

--- ui/streamlit/.ipynb_checkpoints/Home-checkpoint.py
+++ ui/streamlit/.ipynb_checkpoints/Home-checkpoint.py
@@ -12,9 +12,9 @@ st.set_page_config(
 # Display the uploaded image at the top and center
 st.markdown("""
     <div style='text-align: center;'>
-        <img src='data:image/png;base64,{}' width="300">
+        <img src='data:image/png;base64,{}' style='max-width: 100%; max-height: 100%;'>
     </div>
-""".format(st.image(petronas, use_column_width=False)), unsafe_allow_html=True)
+""".format(petronas.decode("utf-8")), unsafe_allow_html=True)
 
 st.markdown("""
     <style>
